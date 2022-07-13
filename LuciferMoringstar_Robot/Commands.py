6895f28ea2c22b2ef86905f782f36157193c5e87
MOVIESCLUB AF <98048092+MOVIESCLUBmy@users.noreply.github.com>
2022-07-13 08:21:35 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 ✨️GᖇOᑌᑭ✨️ 🎖', url='https://t.me/MOVIES_CLUB_GROUP_2')
+                        InlineKeyboardButton('🎖 ✨️GᖇOᑌᑭ✨️ 🎖', url=f'https://t.me/MOVIES_CLUB_GROUP_2')
                     ]
                     ]
                 await bot.send_cached_media(
